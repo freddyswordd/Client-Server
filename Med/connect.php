2722325c8e7c711147ea8bb533974ee47b21bf69
OskarRogild <45284382+OskarRogild@users.noreply.github.com>
2018-11-28 21:09:10 +0100

    forbundet de forskelllige sider

--- Med/connect.php
+++ Med/connect.php
@@ -2,20 +2,21 @@
 
 $inputuser = $_POST["user"];									#låser det vi skriver i det første felt til user
 $inputpass = $_POST["pass"]; 									#låser det vi skriver i det første felt til pass
-$user = "root";
-$password = "";
+#$user = "root";
+#$password = "";
+
+if ($inputuser =="Oskar" && $inputpass == "Roegild0110")
+
+#$connect = mysql_connect("localhost:8443", $user, $password);
+#@mysql_select_db($login) or ("Database ikke fundet");
+
+#$query = "SELECT * FROM 'users' WHERE 'user' = '$inputuser'";
+#$querypass = "SELECT * FROM 'users' WHERE 'password' = '$inputpass'";
+
+#echo $query;
+#echo $querypass;
 
 
-$connect = mysql_connect("localhost:8443", $user, $password);
-@mysql_select_db($login) or ("Database ikke fundet");
-
-$query = "SELECT * FROM 'users' WHERE 'user' = '$inputuser'";
-$querypass = "SELECT * FROM 'users' WHERE 'password' = '$inputpass'";
-
-echo $query;
-echo $querypass;
-
-
-mysql_close()
+#mysql_close()
 
 ?>
