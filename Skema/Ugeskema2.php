daaca84ff00140e6afa27db19be84d8a94e9c26a
Callesen12345 <45283546+Callesen12345@users.noreply.github.com>
2019-01-28 08:45:27 +0100

    Update ugeskema2

--- Skema/Ugeskema2.php
+++ Skema/Ugeskema2.php
@@ -1,26 +1,53 @@
 <!DOCTYPE html>
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
-* {
-  box-sizing: border-box;
-}
 
-body {
-  margin: 0;
-  font-family: Arial, Helvetica, sans-serif;
-}
+.row {
 
-/* The grid: Three equal columns that floats next to each other */
-.column {
-  float: left;
-  width: 33.33%;
-  padding: 50px;
-  text-align: center;
-  font-size: 25px;
+  display: grid;
+  grid-template-columns: auto auto auto auto auto auto auto;
+  grid-gap: 10px;
+  background-color: #A9A9A9;
+  padding: 10px;
   cursor: pointer;
-  color: white;
+}
+
+.row > div {
+  background-color: rgba(255, 255, 255, 0.8);
+  text-align: center;
+  padding: 20px 0;
+  font-size: 20px;
+}
+
+
+.month {
+    padding: 70px 25px;
+    width: 100%;
+    background: #1abc9c;
+    text-align: center;
+}
+
+.month ul {
+    margin: 0;
+    padding: 0;
+}
+
+.month ul li {
+    color: white;
+    font-size: 20px;
+    text-transform: uppercase;
+    letter-spacing: 3px;
+}
+
+.month .prev {
+    float: left;
+    padding-top: 10px;
+}
+
+.month .next {
+    float: right;
+    padding-top: 10px;
 }
 
 .containerTab {
@@ -28,67 +55,93 @@ body {
   color: white;
 }
 
-/* Clear floats after the columns */
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-/* Closable button inside the container tab */
 .closebtn {
   float: right;
   color: white;
-  font-size: 35px;
+  font-size: 100px;
   cursor: pointer;
 }
+
+
 </style>
 </head>
 <body>
 
+<h1>Min Medicin Kalender</h1>
 
-<!-- Three columns -->
+<div class="month">      
+  <ul>
+    <li class="prev">&#10094;</li>
+    <li class="next">&#10095;</li>
+    <li>
+      December<br>
+      <span style="font-size:18px">2018</span>
+    </li>
+  </ul>
+</div>
+
+
 <div class="row">
-  <div class="column" onclick="openTab('b1');" style="background:green;">
-    Box 1
-  </div>
-  <div class="column" onclick="openTab('b2');" style="background:blue;">
-    Box 2
-  </div>
-  <div class="column" onclick="openTab('b3');" style="background:red;">
-    Box 3
-  </div>
+  <div class="item1" onclick="openTab('I1');" style="background:red;">Mandag </div>
+   <div id="I1" class="containerTab" style="display:none;background:red">
+   <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
+   <h2>Mandag</h2>
+   <p>I dag skal du tage din medicin kl. 12:30
+   Mvh Din Læge 
+   </p>
+</div>
+  <div class="item1" onclick="openTab('I2');" style="background:red;">Tirsdag </div>
+   <div id="I2" class="containerTab" style="display:none;background:red">
+   <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
+   <h2>Tirsdag</h2>
+   <p>I dag skal du tage din medicin kl. 12:30
+   Mvh Din Læge 
+   </p>
+</div>
+  <div class="item1" onclick="openTab('I3');" style="background:red;">Onsdag </div>
+  <div id="I3" class="containerTab" style="display:none;background:red">
+   <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
+   <h2>Onsdag</h2>
+   <p>I dag skal du tage din medicin kl. 12:30
+   Mvh Din Læge 
+   </p>
+</div>
+  <div class="item1" onclick="openTab('I4');" style="background:red;">Torsdag </div>
+  <div id="I4" class="containerTab" style="display:none;background:red">
+   <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
+   <h2>Torsdag</h2>
+   <p>I dag skal du tage din medicin kl. 12:30
+   Mvh Din Læge 
+   </p>
+</div>
+  <div class="item1" onclick="openTab('I5');" style="background:red;">Fredag </div>
+  <div id="I5" class="containerTab" style="display:none;background:red">
+   <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
+   <h2>Fredag</h2>
+   <p>I dag skal du tage din medicin kl. 12:30
+   Mvh Din Læge 
+   </p>
+</div>
+  <div class="item1" onclick="openTab('I6');" style="background:red;">Lørdag </div>
+  <div id="I6" class="containerTab" style="display:none;background:red">
+   <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
+   <h2>Lørdag</h2>
+   <p>I dag skal du tage din medicin kl. 12:30
+   Mvh Din Læge 
+   </p>
+</div>
+  <div class="item1" onclick="openTab('I7');" style="background:red;">Søndag </div>
+  <div id="I7" class="containerTab" style="display:none;background:red">
+   <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
+   <h2>Søndag</h2>
+   <p>I dag skal du tage din medicin kl. 12:30
+
+   Mvh Din Læge 
+   </p>
 </div>
 
-<!-- Full-width columns: (hidden by default) -->
-<div id="b1" class="containerTab" style="display:none;background:green">
-  <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
-  <h2>Box 1</h2>
-  <p>Lorem ipsum dolor sit amet, te quo doctus abhorreant, et pri deleniti intellegat, te sanctus inermis ullamcorper nam. Ius error diceret deseruisse ad</p>
-</div>
 
-<div id="b2" class="containerTab" style="display:none;background:blue">
-  <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
-  <h2>Box 2</h2>
-  <p>Lorem ipsum dolor sit amet, te quo doctus abhorreant, et pri deleniti intellegat, te sanctus inermis ullamcorper nam. Ius error diceret deseruisse ad</p>
-</div>
 
-<div id="b3" class="containerTab" style="display:none;background:red">
-  <span onclick="this.parentElement.style.display='none'" class="closebtn">&times;</span>
-  <h2>Box 3</h2>
-  <p>Lorem ipsum dolor sit amet, te quo doctus abhorreant, et pri deleniti intellegat, te sanctus inermis ullamcorper nam. Ius error diceret deseruisse ad</p>
-</div>
-
-<script>
-function openTab(tabName) {
-  var i, x;
-  x = document.getElementsByClassName("containerTab");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  document.getElementById(tabName).style.display = "block";
-}
-</script>
 
 </body>
-</html> 
+</html>
